ac881821fd154ef6cbc7b484aa2f50c53b8ea870
0315candy <0315candy@gmail.com>
2023-04-22 00:15:04 +0800

    test

--- startup-01.py
+++ startup-01.py
@@ -19,6 +19,7 @@ def calculator(num1, operation, num2):
             raise gr.Error("Cannot divide by zero!")
         return num1 / num2
     elif operation == "modulus":
+        return num1 % num2
         # &&&
         pass
     elif operation == "exp":
